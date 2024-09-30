6b0a9bb2e6fbd3b8fde8ff6dd06d7fb810087f4a
ritchieritch42 <ritchie.caleb42@gmail.com>
2024-09-30 18:16:00 -0500

    batman

---
+++ index.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import 'dotenv/config'
+import readline from 'node:readline';
+
+const url = process.env.FRESHDESK_URL;
+const key = process.env.API_KEY;
+
+async function prompt(rl, question) {
+    return(
+        await new Promise(resolve => {
+            rl.question(question, resolve)
+          })
+    )
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const startingTicket = await prompt(rl, "What ticket number do you want to start with?\n")
+const endingTicket = await prompt(rl, "What ticket number do you want to end with?\n")
+let ticketNumber = startingTicket;
+let axiosArray = [];
+
+while (ticketNumber <= endingTicket) {
+    axiosArray.push(
+        axios.get(`${url}tickets/${ticketNumber}/conversations`, {
+            headers: {
+                Authorization: `Basic ${btoa(key)}`,
+                Accept: 'application/json'
+            }
+        })
+    );
+    ticketNumber++;
+}
+
+Promise.all(axiosArray).then((values) => {
+    for(let i = 0; i < values.length; i++) {
+        console.log(JSON.stringify(values[i].data) + values[i].data[0].body_text);
+    }
+  });
+
+rl.close();
